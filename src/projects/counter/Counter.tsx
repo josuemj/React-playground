45505dacec15a67ed9a610f010dfc4605db13cdf
josuemj <98498772+josuemj@users.noreply.github.com>
2025-05-11 21:21:22 -0600

    feat: add Counter component with styling (preview)

--- src/projects/counter/Counter.tsx
+++ src/projects/counter/Counter.tsx
@@ -1,11 +1,12 @@
 import { useState } from "react";
+import "./Counter.css";
 export function Counter() {
   const [count, setCount] = useState(0);
   return (
     <>
       <section className="counter">
         <h1>Counter project</h1>
-        <p>Count: {count}</p>
+        <p>{count}</p>
         <div className="button-group">
           <button onClick={() => setCount(count + 1)}>Increment</button>
           <button onClick={() => setCount(count - 1)}>Decrement</button>
