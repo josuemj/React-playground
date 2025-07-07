0047f3530756342979d75abf18609fde42b0f50d
josuemj <98498772+josuemj@users.noreply.github.com>
2025-07-06 22:51:43 -0600

    feat: implement Tic Tac Toe game logic and styling

--- src/projects/tic-tac-toe/TicTacToe.tsx
+++ src/projects/tic-tac-toe/TicTacToe.tsx
@@ -1,9 +1,87 @@
+import React from "react";
+import { useState } from "react";
+import "./TicTacToe.css"; // Assuming you have a CSS file for styling
 export function TicTacToe() {
+  const [player, setPlayer] = useState("X");
+  const [xPositions, setXPosition] = useState<number[]>([]);
+  const [oPositions, setOPosition] = useState<number[]>([]);
+  const [winner, setWinner] = useState("");
+
+  const play = (player: string, positon: number) => {
+    if (xPositions.includes(positon) || oPositions.includes(positon)) {
+    } else {
+      if (player == "X") {
+        const newXPosition = [...xPositions, positon];
+        setXPosition(newXPosition);
+        if (checkWin(newXPosition)) {
+          setWinner("X");
+        } else if (newXPosition.length + oPositions.length === 9) {
+          setWinner("Draw");
+        }
+        setPlayer("O");
+      } else {
+        const newOPosition = [...oPositions, positon];
+        setOPosition(newOPosition);
+        if (checkWin(newOPosition)) {
+          setWinner("O");
+        } else if (newOPosition.length + xPositions.length === 9) {
+          setWinner("Draw");
+        }
+        setPlayer("X");
+      }
+    }
+  };
+
+  const checkWin = (positions: number[]): boolean => {
+    return wins.some((combo) => combo.every((pos) => positions.includes(pos)));
+  };
+
+  const restart = () => {
+    setXPosition([]);
+    setOPosition([]);
+    setWinner("");
+    setPlayer("X");
+  };
+
+  const wins: number[][] = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 5, 6],
+  ];
+
   return (
     <>
-      <section>
-        <h1>Tic Tac Toe</h1>
-        <p>This is a simple Tic Tac Toe game component.</p>
+      <section className="tic-tac-toe">
+        <h1>Turn {player}</h1>
+        {winner != "" ? <h1>{"Winner! " + winner}</h1> : ""}
+        <div className="board-container">
+          {Array.from({ length: 9 }, (_, i) => (
+            <div
+              className="square"
+              key={i}
+              onClick={() => {
+                play(player, i);
+              }}
+              style={
+                winner != ""
+                  ? { pointerEvents: "none" }
+                  : xPositions.includes(i)
+                  ? { backgroundColor: "red" }
+                  : oPositions.includes(i)
+                  ? { backgroundColor: "blue" }
+                  : {}
+              }
+            >
+              {xPositions.includes(i) ? "X" : oPositions.includes(i) ? "O" : ""}
+            </div>
+          ))}
+        </div>
+        {winner != "" ? <button onClick={restart}>Restart</button> : ""}
       </section>
     </>
   );
